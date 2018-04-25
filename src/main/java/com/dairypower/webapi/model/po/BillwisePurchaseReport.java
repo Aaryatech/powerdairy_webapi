c2671471be4085f7adb2dc2ede44ea25d990844e
ats-12 <ats-12@ats12>
2018-04-25 11:04:42 +0530

    //Mahesh

--- src/main/java/com/dairypower/webapi/model/po/BillwisePurchaseReport.java
+++ src/main/java/com/dairypower/webapi/model/po/BillwisePurchaseReport.java
@@ -5,14 +5,71 @@ import java.sql.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 public class BillwisePurchaseReport {
 	@Id
+	private int poHeaderId;
 	private int poId;
 	private Date poDate;
 	private float poTotal;
 	private int cratesReceivedQty;
 	private String poRemarks;
+	private int shortNo;
+	private int extraNo;
+	private int userId;
+	private int poLeakageQty;
+	private String userName;
+	
+    
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public int getPoHeaderId() {
+		return poHeaderId;
+	}
+
+	public int getShortNo() {
+		return shortNo;
+	}
+
+	public int getExtraNo() {
+		return extraNo;
+	}
+
+	public int getPoLeakageQty() {
+		return poLeakageQty;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setPoHeaderId(int poHeaderId) {
+		this.poHeaderId = poHeaderId;
+	}
+
+	public void setShortNo(int shortNo) {
+		this.shortNo = shortNo;
+	}
+
+	public void setExtraNo(int extraNo) {
+		this.extraNo = extraNo;
+	}
+
+	public void setPoLeakageQty(int poLeakageQty) {
+		this.poLeakageQty = poLeakageQty;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
 	public int getPoId() {
 		return poId;
@@ -21,6 +78,7 @@ public class BillwisePurchaseReport {
 	public void setPoId(int poId) {
 		this.poId = poId;
 	}
+	@JsonFormat(locale = "hi",timezone = "Asia/Kolkata", pattern = "dd-MM-yyyy")
 
 	public Date getPoDate() {
 		return poDate;
--- src/main/java/com/dairypower/webapi/repository/BillwisePurchaseRepository.java
+++ src/main/java/com/dairypower/webapi/repository/BillwisePurchaseRepository.java
@@ -13,19 +13,7 @@ import com.dairypower.webapi.model.po.BillwisePurchaseReport;
 
 public interface BillwisePurchaseRepository extends JpaRepository<BillwisePurchaseReport, Integer> {
 	
-	@Query(value = "select\r\n" + 
-			"t_po_header.po_id,\r\n" + 
-			" t_po_header.po_date,\r\n" + 
-			" t_po_header.po_remarks,\r\n" + 
-			" t_po_header.po_total,\r\n" + 
-			" t_po_header.crates_received_qty\r\n" + 
-			" from\r\n" + 
-			" t_po_header where\r\n" + 
-			" t_po_header.po_date between :fromDate and :toDate \r\n" + 
-			"" 
-			 + 
-		
-			"", nativeQuery = true) 
+	@Query(value ="select t_po_header.po_header_id,t_po_header.po_id,t_po_header.po_date,t_po_header.po_remarks,t_po_header.po_total, t_po_header.crates_received_qty,SUM(t_po_details.short_no) as short_no,SUM(t_po_details.extra_no) as extra_no,SUM(t_po_details.po_leakage_qty) as po_leakage_qty,t_po_header.user_id,m_user.user_name from	 t_po_header,t_po_details,m_user where t_po_details.po_header_id=t_po_header.po_header_id and m_user.user_id=t_po_header.user_id and  t_po_header.po_date between :fromDate and :toDate group by t_po_header.po_header_id", nativeQuery = true) 
 	List<BillwisePurchaseReport> findBillwiseReport(@Param("fromDate") Date fromDate,@Param("toDate") Date toDate);
 
 
